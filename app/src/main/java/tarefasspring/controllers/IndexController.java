cade129b7c2ee40d33f972d061a056b9e2b738e6
luis-cavano <luissevilhadev@gmail.com>
2023-04-25 16:49:28 -0300

    .

--- app/src/main/java/tarefasspring/controllers/IndexController.java
+++ app/src/main/java/tarefasspring/controllers/IndexController.java
@@ -1,5 +1,15 @@
 package tarefasspring.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
 public class IndexController {
-    
+    @RequestMapping("/")
+    public String home(@RequestParam("nome") String nome, Model model){
+        model.addAttribute("menssagem", "Olá "+nome);
+        return "WEB-INF/home.jsp";
+    }
 }
